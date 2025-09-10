0a25926827adfee57667c27aace37e9412338845
GerEspinosa <espinosa.toscano@gmail.com>
2025-09-10 21:24:58 +0200

    Finished Project - Add cart logic

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,11 +2,13 @@ import ProductDetail from '@/components/ProductDetail'
 import { stripe } from '@/lib/stripe'
 import React from 'react'
 
-const ProductPage = async ({params} : {params: {id: string} }) => {
+const ProductPage = async ({params} : {params: Promise<{id: string} >}) => {
+  const {id} = await params
 
-    const product = await stripe.products.retrieve(params.id, {
-        expand: ["default_price"]
+    const product = await stripe.products.retrieve(id, {
+      expand: ["default_price"]
     })
+
 
     const plainProduct = JSON.parse(JSON.stringify(product))
   return <ProductDetail product={plainProduct}/>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,31 @@
+'use client'
+import { useCartStore } from '@/store/cart-store';
+import { ShoppingCartIcon, Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline';
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button';
 
 const Navbar = () => {
+  
+  const [mobileOpen, setMobileOpen] = useState<boolean>(false)
+
+  const {items} = useCartStore() 
+
+  const cartCount = items.reduce((acc, item) => acc + item.quantity, 0)
+
+  // Set mobile effect
+  useEffect(() => {
+    const handleResize = () => {
+      if(window.innerWidth >= 768 ){
+        setMobileOpen(true)
+      }
+    } 
+    window.addEventListener("resize", handleResize)
+
+    return () => window.removeEventListener("resize", handleResize)
+  },[])
+
+
   return (
     <nav className="sticky top-0 z-50 bg-white shadow">
       <div className="container mx-auto flex items-center justify-between px-4 py-4">
@@ -24,8 +48,35 @@ const Navbar = () => {
         </div>
 
         {/* Cart */}
-        <div className="flex items-center space-x-4"></div>
+        <div className="flex items-center space-x-4">
+          <Link href={"/checkout"} className='relative'>
+            <ShoppingCartIcon className='h-6 w-6' />
+            {cartCount > 0 && <span className='absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-red-500'>{cartCount}</span>}
+          </Link>
+
+        {/* Mobile menu */}
+      <Button variant="ghost" className={"md:hidden"}
+      onClick={() => setMobileOpen((prev) => !prev)} >
+        {mobileOpen ? <XMarkIcon className='h-6 w-6'/> : <Bars3Icon className='h-6 w-6'/>}
+      </Button>
+        </div>
       </div>
+
+      {mobileOpen && (
+        <nav className='md:hidden bg-white shadow-md'>
+          <ul className='flex flex-col p-4 space-y-2'>
+            <li className='block hover:text-blue-600'>
+              <Link href={'/'}>Home</Link>
+            </li>
+            <li className='block hover:text-blue-600'>
+              <Link href={'/products'}>Products</Link>
+            </li>
+            <li className='block hover:text-blue-600'>
+              <Link href={'/checkout'}>Checkout</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </nav>
   );
 }
